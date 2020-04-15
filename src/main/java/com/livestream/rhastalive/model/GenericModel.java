b788d823df737a4905a66f9ea67ebba964416dd3
Fábio Veloso <veloso.fabio@hotmail.com>
2020-04-15 15:41:38 +0100

    create product model + update annotations in generic model

--- src/main/java/com/livestream/rhastalive/model/GenericModel.java
+++ src/main/java/com/livestream/rhastalive/model/GenericModel.java
@@ -1,11 +1,32 @@
 package com.livestream.rhastalive.model;
 
-import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@MappedSuperclass
 public abstract class GenericModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
-    private Date creationTime;
-    private Date updateTime;
+
+    @CreationTimestamp
+    @Column(name = "creationTime")
+    private Timestamp creationTime;
+
+    @UpdateTimestamp
+    @Column(name = "updateTime")
+    private Timestamp updateTime;
+
+    @Version
+    @Column(name = "version")
     private Integer version;
 }
