b8596d92b6cab90309250acf63db31956dac8123
Fábio Veloso <veloso.fabio@hotmail.com>
2020-04-16 16:58:15 +0100

    changes during spring to security

--- src/main/java/com/livestream/rhastalive/controller/MainController.java
+++ src/main/java/com/livestream/rhastalive/controller/MainController.java
@@ -12,7 +12,7 @@ public class MainController {
 
     @GetMapping
     public ModelAndView showHomePage() {
-        return new ModelAndView("home");
+        return new ModelAndView("videotest");
     }
 
 }
